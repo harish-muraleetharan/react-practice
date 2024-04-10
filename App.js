14673f4f74b9e6719de44e9102ac36499418c000
harish-muraleetharan <harishmuraleetharan@hotmail.com>
2024-04-10 18:19:43 +0530

    Added a nested component and added npm scripts

--- App.js
+++ App.js
@@ -1,7 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1",{},"Hello World !!!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const H1 = () =>(
+    <h1>
+        Component
+    </h1>
+)
+const H2 = () =>(
+    <h2>
+        Component
+    </h2>
+)
+const H3 = () =>(
+    <h3>
+        Component
+    </h3>
+)
+const H4 = () =>(
+    <h4>
+        Component
+    </h4>
+)
 
-root.render(heading);
+const Headers = () =>(
+    <>
+        <H1/>
+        <H2/>
+        <H3/>
+        <H4/>
+    </>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Headers/>);
